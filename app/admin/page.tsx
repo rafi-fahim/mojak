088927c7690343c238f0e6cd2629a47454a2d050
Fahim Shakil <fsrafi18l@gmail.com>
2023-11-28 22:54:00 +0600

    fixed bug

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -27,7 +27,7 @@ const page = () => {
       <h1 className="text-6xl uppercase pl-2 pr-2 text-center font-medium font-road-rage mb-4 bg-theme-4 border-2 border-theme-1 rounded-[2px] max-sm:w-5/6 w-1/4 ">
         Admin page
       </h1>
-      {checkLogin  ? <AdminPostForm /> : <LoginForm setLogin={trueLoginState} />}
+      {checkLogin ? <AdminPostForm /> : <LoginForm setLogin={trueLoginState} />}
     </div>
   );
 };
